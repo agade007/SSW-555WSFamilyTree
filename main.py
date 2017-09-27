27a2a0a8c05340146e1e0280572c1199146b7140
kunalbhat26 <kbhat1@stevens.edu>
2017-09-27 10:51:14 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+from gedcomFileParser import GEDCOMFileParser
+from prettytable import PrettyTable
+fileName = 'familytree.ged'
+
+x = PrettyTable()
+y = PrettyTable()
+
+def main():
+    individual, families = GEDCOMFileParser(fileName)
+    displayData(individual, families)
+
+def displayData(individual, families):
+
+    # displaying data for individuals
+    x.field_names = ["id","Name","Gender","Birthday","Age","Alive","Death","Child","Spouse"]
+    for line in individual:
+        attrs = vars(line)
+        x.add_row(attrs.values())
+    print("Individuals: ")
+    print(x)
+
+    # displaying data for families
+    y.field_names = ["id","Married","Divorced","Husband ID","Husband Name","Wife ID","Wife Name","Children"]
+    for line in families:
+        attrs = vars(line)
+        y.add_row(attrs.values())
+    print("Families: ")
+    print(y)
+
+if __name__ == '__main__':
+    sys.stdout = open("output.txt","w")
+    main()
+    sys.__stdout__.close()
