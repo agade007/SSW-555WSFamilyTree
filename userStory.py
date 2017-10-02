696e6309546c11f7c69990eff14d6627b261a8d1
tanmaybagwe <tbagwe@stevens.edu>
2017-10-02 16:25:54 -0400

    added user stories 7 and 8

--- userStory.py
+++ userStory.py
@@ -6,6 +6,9 @@ def checkUserStory(individuals, families):
 
     us03_birth_before_death(individuals)
     us04_marriage_before_divorce(families)
+    us07_less_than_150year_old(individuals)
+    us_08_birthbefore_marriage_after9monthsdivorce(individuals,families)
+
 
 #User story 3 birth should occur before death
 def us03_birth_before_death(individuals):
@@ -29,6 +32,42 @@ def us04_marriage_before_divorce(families):
                 report_error('ERROR: FAMILY: US04: ', error_description)
                 return_flag = False
     return return_flag
+
+#user story 7 check if individual lived more than 150 years
+def us07_less_than_150year_old(individuals):
+    return_flag = True
+    today = datetime.now()
+    for individual in individuals:
+
+        if individual.alive == False and relativedelta(individual.death, individual.birthday).years > 150:
+            error_description = individual.id+" lived longer than 150 years"
+            report_error('ANOMALY: INDIVIDUAL: US07: ', error_description)
+            return_flag= False
+        if individual.alive == True and relativedelta(today,individual.birthday).years > 150:
+            error_description = individual.id+" is living longer than 150 years"
+            report_error('ANOMALY: INDIVIDUAL: US07:', error_description)
+            return_flag = False
+    return return_flag
+
+#user story 8 birth before marriage and not after 9 months of divorce
+def us_08_birthbefore_marriage_after9monthsdivorce(individuals, families):
+    return_flag = True
+    for family in families:
+        if family.married:
+            for individual in individuals:
+                id = individual.id
+                bday = individual.birthday
+                if id in family.children and family.divorced is not None:
+                    if bday < family.married:
+                        error_description = "Child "+individual.id+" born "+str(individual.birthday)+" before marriage"+str(family.married)
+                        report_error('ANOMALY: FAMILY: US08:',error_description)
+                        return_flag = False
+                    if relativedelta(bday,family.divorced).months+9:
+                        error_description="Child "+individual.id+" born "+str(individual.birthday)+" after 9 months of divorce on "+str(family.divorced)
+                        report_error('ANOMALY: FAMILY: US08:', error_description)
+                        return_flag=False
+    return return_flag
+
 
 
 def report_error(errortype, description):
