b3bf2291d508225aeea2d541691830c17db359f2
tanmaybagwe <tbagwe@stevens.edu>
2017-10-29 19:50:02 -0400

    added test cases for us35 us36

--- sprint3unittest.py
+++ sprint3unittest.py
@@ -23,6 +23,17 @@ class test_list_living_married_people(unittest.TestCase):
         individuals,families = GEDCOMFileParser(fileName)
         self.assertFalse(us30_list_living_married_people(individuals,families))
 
+class test_people_born_in_30days(unittest.TestCase):
+    def test_people_born_in_30days_1(self):
+        individuals,_ = GEDCOMFileParser(fileName)
+        self.assertFalse(us35_people_born_in_30days(individuals))
+
+class test_people_died_in_last30_days(unittest.TestCase):
+    def test_people_died_in_last30_days_1(self):
+        individuals,_ = GEDCOMFileParser(fileName)
+        self.assertFalse(us36_people_died_in_last30_days(individuals))
+
+
 
 
 if __name__ == '__main__':
